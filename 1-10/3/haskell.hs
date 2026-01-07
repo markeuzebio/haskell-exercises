cc63be3a3b7a34dc962e496f2687ffd82dd28110
markeuzebio <markeuzebio@gmail.com>
2026-01-07 17:33:34 -0300

    Exercise 1-10/3 completed

--- 1-10/3/haskell.hs
+++ 1-10/3/haskell.hs
@@ -1,1 +1,8 @@
+elementAt :: [a] -> Int -> a
+elementAt xs n = head [x | (x,i) <- zip xs [1..], i == n]
 
+-- I think the definition below is more readable than the upper one
+--
+-- elementAt :: [a] -> Int -> a
+-- elementAt (x:xs) 1 = x
+-- elementAt (x:xs) n = elementAt xs (n - 1)
